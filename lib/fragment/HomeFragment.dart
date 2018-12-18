ecdbc2ca519cdd80220822e8b739e21d2c696bbe
zengkai <453055141@qq.com>
2018-12-18 22:58:32 +0800

    bar完成

--- lib/fragment/HomeFragment.dart
+++ lib/fragment/HomeFragment.dart
@@ -26,18 +26,21 @@ class HomeFragmentStart extends State<HomeFragment> {
         appBar: AppBar(
           title: Row(
             children: <Widget>[
-              ClipOval(
-                child: Image.asset(
-                  "images/image/16pic_4875478_b.jpg",
-                  width: 60.0,
-                  height: 40.0,
-                ),
-              ),
+              new CircleAvatar(
+                  backgroundImage: AssetImage("images/16pic_4875478_b.jpg"),
+                  radius: 15.0),
+//              new ClipOval(
+//                child: new Image.asset(
+//                  "images/16pic_4875478_b.jpg",
+//                  width: 35.0,
+//                  height: 38.0,
+//                ),
+//              ),
               Container(
-                margin: EdgeInsets.only(left: 15.0),
+                margin: EdgeInsets.only(left: 15.0, right: 13.0),
                 alignment: Alignment.centerLeft,
                 padding: EdgeInsets.only(left: 4.0),
-                width: 180.0,
+                width: 160.0,
                 height: 30.0,
                 child: Icon(
                   Icons.search,
@@ -48,9 +51,51 @@ class HomeFragmentStart extends State<HomeFragment> {
                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
                 ),
               ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: new Image.asset(
+                  "images/music_icon_subject.png",
+                  width: 35.0,
+                  height: 38.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: new Image.asset(
+                  "images/mipmap-xhdpi-v4/gteq_icon_main_update.png",
+                  width: 35.0,
+                  height: 38.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: new Image.asset(
+                  "images/mipmap-xhdpi-v4/gteq_icon_main_pj.png",
+                  width: 35.0,
+                  height: 38.0,
+                ),
+              ),
             ],
           ),
           centerTitle: true,
+        ),
+        body: new ListView(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("直播"),
+                    Container(
+                      height: 1.0,
+                      width: 30.0,
+                      color: Colors.redAccent,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
